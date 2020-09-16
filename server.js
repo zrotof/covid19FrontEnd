8bf2c4fe6ec5351a481654baf9eb3a5a4a025ea3
Zrotof <manduel21@gmail.com>
2020-09-16 06:54:02 +0200

    Modifying server.js by adding path module

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/front-end-covid19'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/front-end-covid19'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/front-end-covid19' }
-  );
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/front-end-covid19/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
