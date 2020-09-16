3ccdf6121819fba2b1123d3b708285a827a7f2b6
Zrotof <manduel21@gmail.com>
2020-09-16 06:40:56 +0200

    Adding procfile

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/front-end-covid19'));
+app.use(express.static('./dist/front-end-covid19'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/front-end-covid19/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/front-end-covid19' }
+  );
 });
 
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
